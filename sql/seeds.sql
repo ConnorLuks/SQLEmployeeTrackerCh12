8dedaa1d1b695e588a7064310be360d09003c1ad
Connor_OHalloran <HCnamehere@protonmail.com>
2024-07-24 00:15:46 -0400

    fixed seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -34,14 +34,22 @@ CREATE TABLE employee (
 INSERT INTO department (name) VALUES ('Design');
 INSERT INTO department (name) VALUES ('Finance');
 INSERT INTO department (name) VALUES ('Marketing');
+INSERT INTO department (name) VALUES ('IT');
+INSERT INTO department (name) VALUES ('Public Relations');
+INSERT INTO department (name) VALUES ('Distribution');
 
 -- Insert Roles
 INSERT INTO role (title, salary, department_id) VALUES ('Graphics', 50000, 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Economist', 70000, 2);
 INSERT INTO role (title, salary, department_id) VALUES ('Analysis', 60000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('Senior IT Specialist', 85000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ('Public Relations Manager', 45000, 5);
+INSERT INTO role (title, salary, department_id) VALUES ('Lead Warehouse Controller', 60000, 6);
 
 -- Insert Employees
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Connor', 'Jameson', 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Connor', 'Jameson', 1, John Anderson);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Zack', 'OBrian', 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Emily', 'Marion', 3, NULL);
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'James', 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Stan', 'Smith', 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Anderson', 3, NULL);
